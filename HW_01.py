6ad94e7fb0e0b943869070669690d7566b20ea5c
Nmakekung <nmakekung@gmail.com>
2025-05-12 16:02:22 +0700

    update code

--- HW_01.py
+++ HW_01.py
@@ -6,27 +6,37 @@ start = "y"
 score = 0
 com_score = 0
 
+item = ["rock", "paper", "scissors"]
+
+def display_message(result, computer):
+    if result == "win":
+        print("You win! | computer choose " + computer)
+    elif result == "lose":
+        print("You lose! | computer choose " + computer)
+    else:
+        print("Draw! | computer choose " + computer)
+        
 while start == "y":
     user = input("กรุณาเลือก: rock, paper หรือ scissors: ").lower().strip()
 
-    computer = random.choice(["rock", "paper", "scissors"])
+    computer = random.choice(item)
 
-    if user not in ["rock", "paper", "scissors"]:
+    if user not in item:
         print("กรุณาเลือก rock, paper หรือ scissors เท่านั้น")
         continue
     if user == "rock" and computer == "scissors":
-        print("You win! | computer choose " + computer)
+        display_message("win", computer)
         score += 1
     elif user == "scissors" and computer == "paper":
-        print("You win! | computer choose " + computer)
+        display_message("win", computer)
         score += 1
     elif user == "paper" and computer == "rock":
-        print("You win! | computer choose " + computer)
+        display_message("win", computer)
         score += 1
     elif user == computer:
-        print("Draw! | computer choose " + computer)
+        display_message("draw", computer)
     else:
-        print("You lose! | computer choose " + computer)
+        display_message("lose", computer)
         com_score += 1
     
     print("Your score: " + str(score) + " | Computer score: " + str(com_score))
